44549a9e91b2deb95a419e1503cb022ec8859863
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-03-29 23:54:21 +0530

    home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero";
+import AboutShort from "../components/AboutShort";
+import FocusAreas from "../components/FocusAreas";
+import ImpactStats from "../components/ImpactStats";
+import Programs from "../components/Programs";
+import FinalCTA from "../components/FinalCTA";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutShort />
+      <FocusAreas />
+      <ImpactStats />
+      <Programs />
+      <FinalCTA />
+    </>
+  );
+};
+
+export default Home;
